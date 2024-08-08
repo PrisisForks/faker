a019025447b40513c15c6120340c45500eba5b0d
prisis <d.bannert@anolilab.de>
2024-08-08 17:15:06 +0200

    build: switch to packem

--- packem.config.ts
+++ packem.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from '@visulima/packem/config';
 import transformer from '@visulima/packem/transformer/esbuild';
 
 export default defineConfig({
-  declaration: false, // will be generated with the "build:types" script
   minify: true,
   transformer,
-  sourcemap: true,
+  sourcemap: false,
 });
